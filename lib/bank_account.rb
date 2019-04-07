3fe4f1d8916d0d5bc57c0ffcf93f4f9b7132fc00
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-04-07 12:14:51 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,49 @@
+require 'pry'
+
 class BankAccount
 
+  attr_reader :bank_account, :name
+  attr_accessor :balance, :status
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if @balance > 0 && @status === "open"
+      return true
+    elsif @balance > 0 && @status === "closed"
+      return false
+     else
+       return false
+    end
+    binding.pry
+  end
+
+  def close_account
+    if self.valid? == true
+      @status = "open"
+    else
+      @status = "closed"
+    end
+  end
+
 end
+
